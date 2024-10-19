cb1d8f52e9a0c6435f2ed3c147482ef3a1683012
Andrej Sucevic <andrejsucevic@outlook.com>
2024-10-19 14:04:59 +0100

    Comments added to my code
    I added some comments to my code, deleted quit method because it is not used and I moved MenuManager scrip to Scripts file.

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,15 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Starting the game
+    // Starting the game when this method called
     public void PlayGame()
     {
+        // Loads next scene after the current scene
+        // 0 (Main menu scene) + 1 = 1 (Game scene)
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    // Quitting the game
-    public void QuitGame()
-    {
-        Application.Quit();
     }
 }

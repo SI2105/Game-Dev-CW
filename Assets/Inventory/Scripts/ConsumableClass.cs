af4add54250689f9211daf4f1bc9883c3ad2758a
ec22612 <120340281+SI2105@users.noreply.github.com>
2024-12-21 21:26:49 +0000

    Inventory System Completed 50% - Inventory System with Drag Drop Functionality working with ItemClass - Consumable, Weapon, Misc Item Class Created - Consumable, Weapon, Misc Item Scriptable Object Created - HotBar UI created(but not yet functional) - Add, Deletion Capability of Items in Inventory System to be used by player

---
+++ Assets/Inventory/Scripts/ConsumableClass.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Consumable Class", menuName = "Item/Consumable")]
+public class ConsumableClass : ItemClass
+{
+    public override ItemClass GetItem() { return this; }
+    public override WeaponClass GetWeapon() { return null; }
+    public override ConsumableClass GetConsumable() { return this; }
+    public override MiscClass GetMisc() { return null; }
+}
+
---
+++ Assets/Inventory/Scripts/MiscClass.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Misc Class", menuName = "Item/Misc")]
+public class MiscClass : ItemClass
+{
+    public override ItemClass GetItem() { return this; }
+    public override WeaponClass GetWeapon() { return null; }
+    public override ConsumableClass GetConsumable() { return null; }
+    public override MiscClass GetMisc() { return this; }
+}

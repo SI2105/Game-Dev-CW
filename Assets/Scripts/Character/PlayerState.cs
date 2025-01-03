95642ef35257c019fb3efe460db634bcbeae5e63
bubudetp <pro.burak.kose@gmail.com>
2025-01-03 01:01:27 +0100

    Player Collision Handler Fix, Handles Single Click Attack

--- Assets/Scripts/Character/PlayerState.cs
+++ Assets/Scripts/Character/PlayerState.cs
@@ -6,6 +6,7 @@ public class PlayerState : MonoBehaviour
 {
     [field: SerializeField] public PlayerMovementState CurrentPlayerMovementState { get; private set; } = PlayerMovementState.Idling;
     [field: SerializeField] public PlayerAttackState CurrentPlayerAttackState { get; private set; } = PlayerAttackState.Idling;
+    [field: SerializeField] public PlayerAttackStatusState CurrentPlayerAttackStatusState { get; private set; } = PlayerAttackStatusState.Idling;
 
     public void SetPlayerMovementState(PlayerMovementState playerMovementState)
     {
@@ -15,6 +16,11 @@ public class PlayerState : MonoBehaviour
     public void SetPlayerAttackState(PlayerAttackState playerAttackState)
     {
         CurrentPlayerAttackState = playerAttackState;
+    }
+
+    public void SetPlayerAttackStatusState(PlayerAttackStatusState playerAttackStatusState)
+    {
+        CurrentPlayerAttackStatusState = playerAttackStatusState;
     }
 
     public bool InGroundedState()
@@ -30,11 +36,31 @@ public class PlayerState : MonoBehaviour
         // Use the correct property name
         return CurrentPlayerAttackState == state;
     }
+
+    public bool IsInAttackStatusState(PlayerAttackStatusState state){
+        return CurrentPlayerAttackStatusState == state;
+    }
+
+    public bool attack1_progress = false;
+    public bool attack2_progress = false;
+    public bool attack3_progress = false;
+
+    public bool Attack1_progress 
+    {
+        get { return attack1_progress; } 
+        set { attack1_progress = value; }
+    }
+    public bool Attack2_progress 
+    {
+        get { return attack2_progress; } 
+        set { attack2_progress = value; }
+    }
+    public bool Attack3_progress 
+    {
+        get { return attack3_progress; } 
+        set { attack3_progress = value; }
+    }
 }
-
-
-
-
 
 public enum PlayerMovementState{
     Idling = 0,
@@ -48,4 +74,12 @@ public enum PlayerMovementState{
 public enum PlayerAttackState{
     Idling = 0,
     Attacking = 1
+}
+
+public enum PlayerAttackStatusState{
+    InProgress = 0,
+    Idling = 1,
+    Attack_progress_1 = 2,
+    Attack_progress_2 = 3,
+    Attack_progress_3 = 4,
 }
